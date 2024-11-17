5d5cf3e0a1b218a6a7013b0d6c6d5369b40becec
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2024-11-17 16:47:17 +0100

    empty react and spring projects for frontend and backend

---
+++ backend/rank_anything_backend/src/main/kotlin/com/example/rank_anything_backend/RankAnythingBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rank_anything_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RankAnythingBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<RankAnythingBackendApplication>(*args)
+}
